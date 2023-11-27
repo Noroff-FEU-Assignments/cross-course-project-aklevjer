0dda97aec9dc975cee3e54bc3e4810f37c528523
Aleksander Klevjer <alexz_91_@hotmail.com>
2023-11-27 20:26:15 +0100

    Enabled swipe and keyboard navigation for the carousel

--- js/ui/carousel.js
+++ js/ui/carousel.js
@@ -6,6 +6,7 @@
 import * as constants from "../constants/index.js";
 
 let currentPos = constants.slideMinPos;
+let shouldMonitor = true;
 
 function updatePagination() {
   const prevPaginationBtn = document.querySelector(".active-pagination");
@@ -15,9 +16,23 @@ function updatePagination() {
   paginationBtns[currentPos].classList.add("active-pagination");
 }
 
+// Monitor the scroll event to update the current position and pagination if a user is swiping or using keyboard navigation
+function monitorScroll(event) {
+  if (!shouldMonitor) return;
+
+  const slider = event.target;
+  const slideItemWidth = slider.firstElementChild.offsetWidth;
+  const newPosition = slider.scrollLeft;
+
+  currentPos = Math.round(newPosition / (slideItemWidth + constants.slideGap));
+  updatePagination();
+}
+
 function updateSlider() {
+  shouldMonitor = false;
+
   const slider = document.querySelector(".bestsellers-container");
-  const slideItemWidth = slider.querySelector(".product-container").offsetWidth;
+  const slideItemWidth = slider.firstElementChild.offsetWidth;
 
   slider.scrollLeft = currentPos * (slideItemWidth + constants.slideGap);
   updatePagination();
@@ -45,12 +60,15 @@ function nextSlide() {
 }
 
 export function initCarousel() {
+  const slider = document.querySelector(".bestsellers-container");
   const prevBtn = document.querySelector(".carousel-prev");
   const nextBtn = document.querySelector(".carousel-next");
   const paginationBtns = document.querySelectorAll(".carousel-pagination button");
 
   prevBtn.addEventListener("click", prevSlide);
   nextBtn.addEventListener("click", nextSlide);
+  slider.addEventListener("scroll", monitorScroll);
+  slider.addEventListener("scrollend", () => (shouldMonitor = true));
 
   paginationBtns.forEach((btn, i) => {
     btn.addEventListener("click", () => goToSlidePos(i));
